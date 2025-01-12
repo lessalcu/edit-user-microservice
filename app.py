903ca9ddb23c0e98cdab86cade37f3342aafa4ef
Meybili <meybili200219@gmail.com>
2025-01-12 18:07:32 -0500

    refactor: :fire: Update file app.py

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 from flask import Flask
+from flask_cors import CORS
 from config import Config
 from models import db
 from routes import edit_user
 
 # Create the application
 app = Flask(__name__)
+
+CORS(app)
 
 # Configure the application
 app.config.from_object(Config)
@@ -18,4 +21,4 @@ def edit_user_route(id):
     return edit_user(id)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5001, debug=True)
